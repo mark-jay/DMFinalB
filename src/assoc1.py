9606aa7b83b8555accce1129d6929985b1d35e61
fallen <fallen.s4e@gmail.com>
2013-01-12 14:05:39 +0000

    assoc1.py refactored, command line args handling added

--- src/assoc1.py
+++ src/assoc1.py
@@ -4,22 +4,45 @@
 # Classes:     orngAssoc.build, Preprocessor_discretize, EquiNDiscretization
 # Referenced:  assoc.htm
 
-import orange, orngAssoc
+import orange
+import sys
+import orngAssoc
 
-data = orange.ExampleTable("z_datasetSample_201201")
-data = orange.Preprocessor_discretize(data, \
-  method=orange.EquiNDiscretization(numberOfIntervals=3))
-data = data.select(range(10))
+def tryParseFloat(s, val=None):
+  try:
+    return float(s)
+  except ValueError:
+    return val
 
-rules = orange.AssociationRulesInducer(data, support=0.4)
+defaultSupport = 0.9
 
-print "%i rules with support higher than or equal to %5.3f found.\n" % (len(rules), 0.4)
+def getSupport():
+    if len( sys.argv ) > 1:
+        return tryParseFloat( sys.argv[1], val = defaultSupport )
+    return defaultSupport
 
-orngAssoc.sort(rules, ["support", "confidence"])
+def usage():
+    return ("The first parameter is a minimum support which equals %s if it is not supported" % defaultSupport)
 
-orngAssoc.printRules(rules[:5], ["support", "confidence"])
-print
+print usage()
 
-del rules[:3]
-orngAssoc.printRules(rules[:5], ["support", "confidence"])
-print
+def main():
+    data = orange.ExampleTable("../data/zz_datasetSample_201201_for_Orange.txt")
+    data = orange.Preprocessor_discretize(data, \
+      method=orange.EquiNDiscretization(numberOfIntervals=3))
+    data = data.select(range(10))
+
+    rules = orange.AssociationRulesInducer(data, support=getSupport())
+
+    print "%i rules with support higher than or equal to %5.3f found.\n" % (len(rules), getSupport())
+
+    orngAssoc.sort(rules, ["support", "confidence"])
+
+    orngAssoc.printRules(rules[:5], ["support", "confidence"])
+    print
+
+    del rules[:3]
+    orngAssoc.printRules(rules[:5], ["support", "confidence"])
+    print
+
+main()
