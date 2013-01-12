3f00e124f6bc56df922db3140bb3240f89f090a5
fallen <fallen.s4e@gmail.com>
2013-01-12 14:48:41 +0000

    assoc1.py now reads file from command line, refactored

--- src/assoc1.py
+++ src/assoc1.py
@@ -17,17 +17,23 @@ def tryParseFloat(s, val=None):
 defaultSupport = 0.9
 
 def getSupport():
-    if len( sys.argv ) > 1:
-        return tryParseFloat( sys.argv[1], val = defaultSupport )
+    if len( sys.argv ) > 2:
+        return tryParseFloat( sys.argv[2], val = defaultSupport )
     return defaultSupport
 
-def usage():
-    return ("The first parameter is a minimum support which equals %s if it is not supported" % defaultSupport)
+def getFile():
+    if len( sys.argv ) > 1:
+        return sys.argv[1]
+    print usage()
+    sys.exit()
 
-print usage()
+def usage():
+    return ("The first parameter is filepath, and the second is a minimum support which equals %s if it is not supported" % defaultSupport)
 
 def main():
-    data = orange.ExampleTable("../data/zz_datasetSample_201201_for_Orange.txt")
+    print ("searching in a file '%s' ..." % getFile())
+    print
+    data = orange.ExampleTable(getFile())
     data = orange.Preprocessor_discretize(data, \
       method=orange.EquiNDiscretization(numberOfIntervals=3))
     data = data.select(range(10))
@@ -38,11 +44,7 @@ def main():
 
     orngAssoc.sort(rules, ["support", "confidence"])
 
-    orngAssoc.printRules(rules[:5], ["support", "confidence"])
-    print
-
-    del rules[:3]
-    orngAssoc.printRules(rules[:5], ["support", "confidence"])
+    orngAssoc.printRules(rules[:40], ["support", "confidence"])
     print
 
 main()
